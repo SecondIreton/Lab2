7007e1191eb14e525749473569263dfbace1bd25
Trevor Foley <trevormfoley@gmail.com>
2023-01-31 13:03:10 -0800

    setters for clCont class

--- src/closed_loop_control.py
+++ src/closed_loop_control.py
@@ -2,6 +2,16 @@
 """
 Created on Tue Jan 31 12:44:02 2023
 
-@author: gabri
+@author: gabriel, Trevor Foley
 """
+
+class clCont:
+    setpoint = 0
+    Kp = 0
+
+    def set_setpoint(self, newSetpoint):
+        self.setpoint = newSetpoint
+
+    def set_Kp(self, newKp):
+        self.Kp = newKp
 
