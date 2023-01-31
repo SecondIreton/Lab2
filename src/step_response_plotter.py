922818bdefc336cff30df8bacb06f12c915e5be8
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-01-31 14:52:05 -0800

    Update step_response_plotter.py

--- src/step_response_plotter.py
+++ src/step_response_plotter.py
@@ -11,23 +11,26 @@ import serial
 # Import pyplot
 from matplotlib import pyplot
 
-# Create lists of x and y data
+# Create empty lists of x and y data
 x_data = []
 y_data = []
 
 # Create labels for plots
-x_label = 'Time, s'  ## 
-y_label = 'Location, ticks'
+x_label = 'Time, s'  ## Variable name
+y_label = 'Location, ticks'  ## Variable name
 
+req_point =  input('Enter a 4 digit number: ')
+if req_point.type == int():
+    setpoint = req_point  # Assign the inputed value to setpoint
+    
 # The shoe serial port for the mac: /dev/tty.usbmodem2073337757522
 
 with serial.Serial() as s_port: # Fill in correct serial port when found
     
-    s_port.write (b 'Test') # Send the set point value
+    s_port.write (setpoint) # Send the set point value
     
-    
-    print(s_port.readline().split(b ',')) # Test case to see if we have the correct inputs
-    entry = s_port.readline().split(b ',')
+    entry = s_port.readline().split(b',')
+    print(entry) # Test to see if we have the correct inputs
     try:    # Check if the values can be converted into a float
         entry[0] = float(entry[0])
         entry[1] = float(entry[1])
